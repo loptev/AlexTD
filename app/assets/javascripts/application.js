59bb2c6e20228da6ef46617b1ec30e5b13bd36bf
TheJKFever <Jon.Koehmstedt@gmail.com>
2014-04-15 21:11:40 -0700

    after session 20140415

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery-2.0.3
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
