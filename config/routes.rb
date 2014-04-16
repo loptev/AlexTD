b6cce7abc11260ea57ddf556b336b3de0d017417
TheJKFever <Jon.Koehmstedt@gmail.com>
2014-04-15 19:03:34 -0700

    reformatted things

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Railstowerdefense::Application.routes.draw do
   get "logout", to: "sessions#logout"
   get "signup", to: "users#new"
   get "scoreboard", to: "home#scoreboard"
+
+  get 'test_page', to: "home#test_page"
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
